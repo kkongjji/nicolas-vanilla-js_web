86033b1a197e4558223a17e7c0ffce5d325f960d
kkongjji <76424788+kkongjji@users.noreply.github.com>
2022-06-08 02:06:30 +0900

    Add files via upload
    니콜라스 바닐라 자바스크립트만 쓴 웹사이트

---
+++ weather.js
@@ -0,0 +1,22 @@
+const weather = document.querySelector("#weather span:first-child");
+const city = document.querySelector("#weather span:last-child");
+const API_KEY = "4b768c23e1035b81eed1e99fc9ac3bc8";
+
+//위도 경도 api키를 구해서 날씨 api를 동작
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)//URL을 fetch함
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
